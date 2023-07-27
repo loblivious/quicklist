d4bed4defb0bf3280abd3939e0115ac85f2a9a26
B10054 <yjluo@tdcc.com.tw>
2023-07-27 17:35:50 +0800

    create checklist items component - half-way

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -2,8 +2,9 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { switchMap } from 'rxjs';
+import { BehaviorSubject, switchMap } from 'rxjs';
 import { ChecklistService } from '../shared/data-access/checklist.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checklist',
@@ -29,10 +30,16 @@ export class ChecklistComponent {
       this.checklistService.getChecklistById(paramMap.get('id') as string)
     )
   );
+  formModalIsOpen$ = new BehaviorSubject<boolean>(false);
+
+  checklistItemForm = this.fb.nonNullable.group({
+    title: ['', Validators.required],
+  });
 
   constructor(
     private route: ActivatedRoute,
-    private checklistService: ChecklistService
+    private checklistService: ChecklistService,
+    private fb: FormBuilder
   ) {}
 }
 
---
+++ src/app/checklist/data-access/checklist-item.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import {
+  AddChecklistItem,
+  ChecklistItem,
+} from 'src/app/shared/interfaces/checklist-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChecklistItemService {
+  checklistItems$ = new BehaviorSubject<ChecklistItem[]>([]);
+
+  getItemsByChecklistId(checklistId: string) {
+    return this.checklistItems$.pipe(
+      map((items) => items.filter((item) => item.checklistId === checklistId))
+    );
+  }
+
+  add(item: AddChecklistItem, checklistId: string) {
+    const newItem = {
+      id: Date.now().toString(),
+      checklistId,
+      checked: false,
+      ...item,
+    };
+
+    this.checklistItems$.next([...this.checklistItems$.value, newItem]);
+  }
+}

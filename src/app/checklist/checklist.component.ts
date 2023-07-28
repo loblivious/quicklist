f9ae355f70fb2338f65fafe1c2d6e96c487ea0bc
B10054 <yjluo@tdcc.com.tw>
2023-07-28 10:44:46 +0800

    created checklist item component

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,36 +1,80 @@
+import { ChecklistItemService } from './data-access/checklist-item.service';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { BehaviorSubject, switchMap } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, map, switchMap } from 'rxjs';
 import { ChecklistService } from '../shared/data-access/checklist.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormModalComponentModule } from '../shared/ui/form-modal.component';
+import { ChecklistItemListComponentModule } from './ui/checklist-item-list.component';
+import { Checklist } from '../shared/interfaces/checcklist';
 
 @Component({
   selector: 'app-checklist',
   template: `
-    <ion-header>
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-back-button defaultHref="/"></ion-back-button>
-        </ion-buttons>
-        <ion-title *ngIf="checklist$ | async as checklist">{{
-          checklist.title
-        }}</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <ng-container *ngIf="vm$ | async as vm">
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-back-button defaultHref="/"></ion-back-button>
+          </ion-buttons>
+          <ion-title>
+            {{ vm.checklist.title }}
+          </ion-title>
+          <ion-buttons slot="end">
+            <ion-button (click)="formModalIsOpen$.next(true)">
+              <ion-icon name="add" slot="icon-only"></ion-icon>
+            </ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
 
-    <ion-content></ion-content>
+      <ion-content>
+        <app-checklist-item-list
+          [checklistItems]="vm.items"
+        ></app-checklist-item-list>
+        <ion-modal
+          [isOpen]="vm.formModalIsOpen"
+          [canDismiss]="true"
+          (ionModalDidDismiss)="formModalIsOpen$.next(false)"
+        >
+          <ng-template>
+            <app-form-modal
+              title="Create item"
+              [formGroup]="checklistItemForm"
+              (save)="addChecklistItem(vm.checklist.id)"
+            ></app-form-modal>
+          </ng-template>
+        </ion-modal>
+      </ion-content>
+    </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChecklistComponent {
-  checklist$ = this.route.paramMap.pipe(
+  checklistAndItems$ = this.route.paramMap.pipe(
     switchMap((paramMap) =>
-      this.checklistService.getChecklistById(paramMap.get('id') as string)
+      combineLatest([
+        this.checklistService
+          .getChecklistById(paramMap.get('id') as string)
+          .pipe(filter((checklist): checklist is Checklist => !!checklist)),
+        this.checklistItemService.getItemsByChecklistId(
+          paramMap.get('id') as string
+        ),
+      ])
     )
   );
+
   formModalIsOpen$ = new BehaviorSubject<boolean>(false);
+
+  vm$ = combineLatest([this.checklistAndItems$, this.formModalIsOpen$]).pipe(
+    map(([[checklist, items], formModalIsOpen]) => ({
+      checklist,
+      items,
+      formModalIsOpen,
+    }))
+  );
 
   checklistItemForm = this.fb.nonNullable.group({
     title: ['', Validators.required],
@@ -38,9 +82,17 @@ export class ChecklistComponent {
 
   constructor(
     private route: ActivatedRoute,
+    private fb: FormBuilder,
     private checklistService: ChecklistService,
-    private fb: FormBuilder
+    private checklistItemService: ChecklistItemService
   ) {}
+
+  addChecklistItem(checklistId: string) {
+    this.checklistItemService.add(
+      this.checklistItemForm.getRawValue(),
+      checklistId
+    );
+  }
 }
 
 @NgModule({
@@ -48,6 +100,8 @@ export class ChecklistComponent {
   imports: [
     CommonModule,
     IonicModule,
+    FormModalComponentModule,
+    ChecklistItemListComponentModule,
     RouterModule.forChild([
       {
         path: '',
